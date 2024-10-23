ad77e5dfa82d99e48d90d73e85648e53bfb10ee2
PERERA WJR <96611644+JanithRavisanka@users.noreply.github.com>
2024-10-23 09:53:50 +0530

    Update email and social media endpoints in config.ts

--- src/app/app-settings/config.ts
+++ src/app/app-settings/config.ts
@@ -11,5 +11,6 @@ export const emailEndpoint = 'http://localhost:8010/email/v2';
 
 // export const apiEndpoint = 'http://13.201.50.150:8000'
 
-export const socialMediaBackendAPI = 'https://icsms-social-media-backe-git-23d52d-tharindu-sathsaras-projects.vercel.app'
-  // export const socialMediaBackendAPI = 'http://localhost:8000'
+// export const socialMediaBackendAPI = 'https://icsms-social-media-backe-git-23d52d-tharindu-sathsaras-projects.vercel.app'
+  export const socialMediaBackendAPI = 'http://localhost:8020'
+  
